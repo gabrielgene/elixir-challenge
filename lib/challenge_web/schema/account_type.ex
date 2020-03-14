fae2f63355be1d16db672c9d385d8564644bd2ed
Gabriel Gene <gabrielgene97@gmail.com>
2020-03-14 16:33:23 -0300

    Add transactions in account type

--- lib/challenge_web/schema/account_type.ex
+++ lib/challenge_web/schema/account_type.ex
@@ -1,9 +1,14 @@
 defmodule ChallengeWeb.Schema.AccountTypes do
   use Absinthe.Schema.Notation
 
+  alias ChallengeWeb.Resolvers
+
   @desc "A account of the bank"
   object :account do
     field :id, :id
     field :current_balance, :float
+    field :transactions, list_of(:transaction) do
+      resolve &Resolvers.Transactions.list_transactions/3
+    end
   end
 end
