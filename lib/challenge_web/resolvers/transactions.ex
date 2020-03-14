2efdab74a112d96a8bfebeb721590148e01dde40
Gabriel Gene <gabrielgene97@gmail.com>
2020-03-14 17:04:11 -0300

    Add transactions specs

--- lib/challenge_web/resolvers/transactions.ex
+++ lib/challenge_web/resolvers/transactions.ex
@@ -1,12 +1,47 @@
 defmodule ChallengeWeb.Resolvers.Transactions do
 
-  def create_transaction(_parent, args, _resolution) do
-    case Challenge.Bank.create_transaction(args) do
-      {:ok, transaction} ->
-        {:ok, transaction}
-      _error ->
-        {:error, "could not create transaction"}
+  def create_transaction(_parent, %{sender: sender, address: address, amount: amount}, _resolution) do
+    try do
+      if sender === address do
+        {:error, "Sender and address dont should be the same"}
       end
+
+      sender_account = case Challenge.Bank.find_account(sender) do
+        nil ->
+          raise "Account ID #{sender} not found"
+        user -> user
+      end
+
+      if sender_account.current_balance < amount do
+        raise "Insufficient amount of money in the sender account #{sender_account.id}"
+      end
+
+      address_account = case Challenge.Bank.find_account(address) do
+        nil ->
+          raise "Account ID #{address} not found"
+        user -> user
+      end
+
+      case Challenge.Bank.update_account(sender_account, %{current_balance: sender_account.current_balance - amount}) do
+        nil -> raise "Error on update sender account #{sender_account.id}"
+        user -> user
+      end
+
+      case Challenge.Bank.update_account(address_account, %{current_balance: address_account.current_balance + amount}) do
+        nil -> raise "Error on update address account #{address_account.id}"
+        user -> user
+      end
+
+      case Challenge.Bank.create_transaction(%{sender: sender, amount: amount, address: address}) do
+        {:ok, transaction} ->
+          {:ok, transaction}
+        _error ->
+          {:error, "Could not create transaction"}
+      end
+
+    rescue
+      e in RuntimeError -> {:error, "#{e.message}"}
+    end
   end
 
   def list_transactions(%{id: id}, _args, _resolution) do
@@ -17,5 +52,4 @@ defmodule ChallengeWeb.Resolvers.Transactions do
         {:ok, transactions}
     end
   end
-
 end
